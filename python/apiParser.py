790ac83119a9a656f1c82ceaf50e67db22074048
yunyezl <69361613+yunyezl@users.noreply.github.com>
2020-12-11 21:52:17 +0900

    Update apiParser.py

--- python/apiParser.py
+++ python/apiParser.py
@@ -42,7 +42,7 @@ year = [str("%02d" %(y)) for y in range(2018, 2021)]
 month = [str("%02d" %(m)) for m in range(1, 13)]
 base_date_list = ["%s%s" %(y, m) for y in year for m in month ]
 
-gu_list = ['강북구']
+gu_list = ['강남구', '강북구', '강동구', '강서구', '관악구', '광진구', '구로구', '금천구', '노원구', '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구', '성동구', '성북구', '송파구', '영등포구', '양천구', '용산구', '은평구', '종로구', '중구', '중랑구']
 for gu in gu_list:
     gu_code = code[ (code['name'].str.contains(gu))]
     gu_code = gu_code['code'].reset_index(drop=True)
